a2055191100b4b9cc8775ff7cf48e1c4d795cb0e
Daniil Yakovlev <danilyakovlev2009@gmail.com>
2016-03-24 14:05:05 +0200

    Forgot to add main.cpp to the stage area)

---
+++ 2.2/lab_2.2.3/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "Integer.h"
+using namespace std;
+
+int main()
+{
+    int arr1[] = {10, 20, 144};
+    int arr2[] = {1, 2, 6};
+    Integer integer1(arr1);
+    Integer integer2(arr2);
+    Integer integer3();
+    cout << "Demonstration of ostream(<<) overloading: " << endl;
+    cout << "integer1: ";
+    cout << integer1;
+    cout << "integer2: ";
+    cout << integer2 << endl;
+
+    cout << "Demonstration of \'==\' operator overloading: " << endl;
+    cout << "integer1 == integer2 : ";
+    cout << (integer1 == integer2) << endl;
+    cout << "Demonstration of \'!=\' operator overloading: " << endl;
+    cout << "integer1 != integer2 : ";
+    cout << (integer1 != integer2) << endl;
+
+    cout << "Demonstration of \'[]\' operator overloading: " << endl;
+    cout << "integer1[2] : ";
+    cout << integer1[2] << endl;
+
+    cout << "Demonstration of istream(>>) overloading: " << endl;
+    cin >> integer3;
+
+    cin.get();
+    return 0;
+}
+
+
+
+
