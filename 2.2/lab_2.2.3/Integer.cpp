ef667a345745fce27983afeac9d5ec8b61c480e6
Daniil Yakovlev <danilyakovlev2009@gmail.com>
2016-03-24 14:02:40 +0200

    Done, but have a bug with istream operator overloading

---
+++ 2.2/lab_2.2.3/Integer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by yadaniil on 3/23/16.
+//
+
+#include "Integer.h"
---
+++ 2.2/lab_2.2.3/Integer.h
@@ -0,0 +1,14 @@
+//
+// Created by yadaniil on 3/23/16.
+//
+
+#ifndef LAB_2_2_3_INTEGER_H
+#define LAB_2_2_3_INTEGER_H
+
+
+class Integer {
+
+};
+
+
+#endif //LAB_2_2_3_INTEGER_H
